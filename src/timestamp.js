9b64b9c1394fb6e895c5fb29c020bd89a277544b
x-rodionov <code512edu@gmail.com>
2022-07-02 23:28:06 +0530

    bugs fixed

--- src/timestamp.js
+++ src/timestamp.js
@@ -1,12 +1,11 @@
-import now as nowInMicroseconds from "microtime";
+import {now as nowInMicroseconds} from "microtime";
 
 function timestamp() {
   // More information on timestamp retrieval: https://stackoverflow.com/a/18197438/7346359  
-  const loadTimeInMilliseconds = Date.now();
-  // Note: to get precision in microseconds the time below should be multiplied by a 1000,
+  // Note: to get precision in microseconds the time below should be used as is,
   // but that adds 1 superfluous integer at the end which cannot be used in the COMB.
-  // Therefore it must be multiplied by only a 100 (meaing centimilli seconds)
-  const time = (loadTimeInMilliseconds + 0.001 * nowInMicroseconds()) * 100;
+  // Therefore it must be multiplied by 0.1 (meaing centimilli seconds)
+  const time = 0.1 * nowInMicroseconds();
   // Round to remove unnecessary digits
   return Math.round(time);
 }
