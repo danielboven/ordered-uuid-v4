4e2b7cefc439000604cd01bf2e7a1568860394d8
x-rodionov <code512edu@gmail.com>
2022-07-02 22:22:53 +0530

    issue #1 fixed, replace performance-now by microtime
    https://github.com/danielboven/ordered-uuid-v4/issues/1

--- src/timestamp.js
+++ src/timestamp.js
@@ -1,12 +1,12 @@
-import now from "performance-now";
+import now as nowInMicroseconds from "microtime";
 
 function timestamp() {
   // More information on timestamp retrieval: https://stackoverflow.com/a/18197438/7346359  
-  const loadTimeInMS = Date.now();
+  const loadTimeInMilliseconds = Date.now();
   // Note: to get precision in microseconds the time below should be multiplied by a 1000,
   // but that adds 1 superfluous integer at the end which cannot be used in the COMB.
   // Therefore it must be multiplied by only a 100 (meaing centimilli seconds)
-  const time = (loadTimeInMS + now()) * 100;
+  const time = (loadTimeInMilliseconds + 0.001 * nowInMicroseconds()) * 100;
   // Round to remove unnecessary digits
   return Math.round(time);
 }
